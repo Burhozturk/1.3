433b7bda05d279957f81d905acf4852e0240c999
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-09-20 11:43:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("aaa   v   vaaa");
+        System.out.println(" )aa   vv   a a");
+        System.out.println("aaaa    v   aaaa");
+
+    }
+
+
+}
